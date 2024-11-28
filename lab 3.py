6cc0dbe98f49c33bc86fc3a88d2a97474cc6a6d5
Sangavialbert <sangavialbert015@gmail.com>
2024-11-28 14:34:33 +0530

    Add files via upload

---
+++ lab 3.py
@@ -0,0 +1,21 @@
+print("CIRCLE")
+r=int(input("Enter a radius: "))
+ans1=3.14*(r*r)
+print("Area of circle: ",ans)
+
+print("RECTANGLE")
+l=int(input("Enter a length: "))
+b=int(input("Enter a breadth: "))
+ans2=l*b
+print("Area of rectangle: ",ans2)
+
+print("TRIANGLE")
+l=int(input("Enter a length: "))
+b=int(input("Enter a breadth: "))
+ans3=(1/2)*l*b
+print("Area of triangle: ",ans3)
+
+print("SQUARE")
+a=int(input("Enter a area: "))
+ans4=a*a
+print("Area of square: ",ans4)
