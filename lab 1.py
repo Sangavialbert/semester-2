4a2da3aa7f6ac2c335129861dc029aad11ef9c8a
Sangavialbert <sangavialbert015@gmail.com>
2024-11-28 12:22:05 +0530

    Add files via upload

---
+++ lab 1.py
@@ -0,0 +1,13 @@
+print("Enter 'c' to connect from Celsius to Fahrenheit")
+print("Enter 'f' to convert from Fahrenheit to Celsius")
+choice = input("Enter your choice: ")
+if choice == 'c':
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9/5)+32
+    print('%.2fCelsius is: %0.2fFahrenheit'%(celsius,fahrenheit))
+elif choice == 'f':
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = (fahrenheit - 32)*5/9
+    print('%.2fFahrenheit is: %0.2fCelsius'%(fahrenheit,celsius))
+else:
+    print('Invalid Input')
